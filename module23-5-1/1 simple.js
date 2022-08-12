a160749350487ec800a7ef9cce6998804f59af42
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-12 12:20:23 +0600

    1 simple.js

---
+++ module23-5-1/1 simple.js
@@ -0,0 +1,13 @@
+/* for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+ */
+function sum(i) {
+  console.log(i);
+  return i;
+}
+
+const result = sum(5);
+console.log(result);
+
+// function sum(i) {}
