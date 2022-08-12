264928467a81563f0fec738c1ccf8c32c95d517d
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-12 09:58:17 +0000

    simple.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # milestone4-module23-5-basic-js-bonus
+
+- 23_5-1 Understand recursion using sum of numbers
+- 23_5-2
--- module23-5-1/1 simple.js
+++ module23-5-1/1 simple.js
@@ -1,13 +1,51 @@
 /* for (let i = 1; i <= 10; i++) {
   console.log(i);
+} */
+/* 
+let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 5; i++) {
+  sum += i;
 }
+console.log(sum);
  */
+/* 
+let sum = 0;
+
+for (let i = 5; i >= 1; i--) {
+  sum += i;
+}
+console.log(sum);
+ */
+/* 
+function sum(i) {
+  console.log(i);
+  return i + sum(i - 1);
+}
+
+const result = sum(5);
+console.log(result);
+ */
+
+function sum(i) {
+  if (i === 1) {
+    return 1;
+  }
+  return i + sum(i - 1);
+  // console.log(i);
+  // return i + sum(i - 1);
+}
+
+const result = sum(5);
+console.log(result);
+
+/* 
 function sum(i) {
   console.log(i);
   return i;
 }
 
 const result = sum(5);
-console.log(result);
+console.log(result); */
 
 // function sum(i) {}
